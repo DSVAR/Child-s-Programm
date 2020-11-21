8a99d364f682a30066a3ec6f5f097fd723e2033e
DVAR <danilka070103@gmail.com>
2020-11-21 11:53:39 +0300

    Добавьте файлы проекта.

---
+++ Child's Programm/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace Child_s_Programm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.License = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Numbers = new System.Windows.Forms.TabPage();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.monthCalendar2 = new System.Windows.Forms.MonthCalendar();
+            this.tabControl1.SuspendLayout();
+            this.License.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.Numbers.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.License);
+            this.tabControl1.Controls.Add(this.Numbers);
+            this.tabControl1.Location = new System.Drawing.Point(2, 2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(515, 436);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // License
+            // 
+            this.License.BackgroundImage = global::Child_s_Programm.Properties.Resources.Права;
+            this.License.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.License.Controls.Add(this.monthCalendar2);
+            this.License.Controls.Add(this.monthCalendar1);
+            this.License.Controls.Add(this.textBox2);
+            this.License.Controls.Add(this.pictureBox1);
+            this.License.Controls.Add(this.textBox1);
+            this.License.Location = new System.Drawing.Point(4, 22);
+            this.License.Name = "License";
+            this.License.Padding = new System.Windows.Forms.Padding(3);
+            this.License.Size = new System.Drawing.Size(507, 410);
+            this.License.TabIndex = 0;
+            this.License.Text = "Права";
+            this.License.UseVisualStyleBackColor = true;
+            this.License.Click += new System.EventHandler(this.License_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(6, 76);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(143, 222);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(256, 104);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(222, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Numbers
+            // 
+            this.Numbers.Controls.Add(this.pictureBox2);
+            this.Numbers.Location = new System.Drawing.Point(4, 22);
+            this.Numbers.Name = "Numbers";
+            this.Numbers.Padding = new System.Windows.Forms.Padding(3);
+            this.Numbers.Size = new System.Drawing.Size(507, 410);
+            this.Numbers.TabIndex = 1;
+            this.Numbers.Text = "Номера";
+            this.Numbers.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(24, 15);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(218, 237);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(535, 400);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(209, 147);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(269, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(161, 254);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.SelectionRange = new System.Windows.Forms.SelectionRange(new System.DateTime(2013, 11, 21, 0, 0, 0, 0), new System.DateTime(2013, 11, 27, 0, 0, 0, 0));
+            this.monthCalendar1.TabIndex = 3;
+            this.monthCalendar1.TodayDate = new System.DateTime(2020, 11, 21, 0, 0, 0, 0);
+            // 
+            // monthCalendar2
+            // 
+            this.monthCalendar2.Location = new System.Drawing.Point(329, 254);
+            this.monthCalendar2.Name = "monthCalendar2";
+            this.monthCalendar2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 443);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.License.ResumeLayout(false);
+            this.License.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Numbers.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage License;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabPage Numbers;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.MonthCalendar monthCalendar2;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+    }
+}
+
---
+++ Child's Programm/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Child_s_Programm
+{
+    public partial class Form1 : Form
+    {
+        Bitmap Photo;
+        Bitmap Licenses = Properties.Resources.license;
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap Result;
+    
+            OpenFileDialog openFile = new OpenFileDialog();
+            SaveFileDialog saveFile = new SaveFileDialog();
+            openFile.Filter = "Файлы изображения | *.bmp;*.png;*jpg;";
+            if (openFile.ShowDialog() == DialogResult.Cancel)
+                return;
+
+            if (openFile.FileName != null)
+            {
+            //    pictureBox2.Image = Image.FromFile(openFile.FileName);
+                Photo = new Bitmap(Image.FromFile(openFile.FileName));
+                //          Bitmap result = new Bitmap()
+                pictureBox2.Image = Photo;
+             //   Result = new Bitmap(Licenses, License.Width, License.Height);
+                Result = new Bitmap(License.Width,License.Height,System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+                Graphics grap = Graphics.FromImage(Result);
+                grap.CompositingMode = System.Drawing.Drawing2D.CompositingMode.SourceOver;
+
+
+                Photo = new Bitmap(Photo, new Size(300, 300));
+                grap.DrawImage(Licenses,0,0);
+                grap.DrawImage(Photo, (License.Width-Photo.Width) / 2, (License.Height-Photo.Height) / 2, Photo.Width, Photo.Height);
+                pictureBox1.Image = Result;
+
+                Result.Save("image.Jpeg", System.Drawing.Imaging.ImageFormat.Jpeg);
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void License_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
